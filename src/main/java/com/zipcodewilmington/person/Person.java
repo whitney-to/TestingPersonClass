3be826df3537834cad569c5dcdad4b9ef95e667f
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-08 10:57:29 -0500

    added fields and tests for fields

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,6 +7,13 @@ public class Person {
     private String name;
     private int age;
 
+    //Adding 5 more fields
+    private double height;
+    private String job;
+    private String[] hobbies;
+    private char gender;
+    private String eyeColor;
+
     public Person() {
         this.name = "";
         this.age = Integer.MAX_VALUE;
@@ -39,5 +46,55 @@ public class Person {
 
     public Integer getAge() {
         return this.age;
+    }
+
+    public Person(String name, int age, double height, String job, String[] hobbies, char gender, String eyeColor) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.job = job;
+        this.hobbies = hobbies;
+        this.gender = gender;
+        this.eyeColor = eyeColor;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -3,6 +3,8 @@ package com.zipcodewilmington.person;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 2/12/18.
  */
@@ -94,5 +96,72 @@ public class TestPerson {
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorWithAllFields() {
+        Person person  = new Person("John",
+                25, 5.4, "Developer",
+                new String[]{"Gaming", "Reading"},
+                'M', "Black");
+
+        boolean actual = person.getName().equals("John")
+                        && person.getAge() == 25
+                        && person.getHeight() == 5.4
+                        && person.getJob().equals("Developer")
+                        && Arrays.equals(person.getHobbies(), new String[]{"Gaming", "Reading"})
+                        && person.getGender() == 'M'
+                        && person.getEyeColor().equals("Black");
+
+        Assert.assertTrue(actual);
+        }
+    @Test
+    public void testGetHeight() {
+        Person person = new Person();
+        person.setHeight(5.7);
+
+        double expected = 5.7;
+        double actual = person.getHeight();
+        Assert.assertEquals(expected, actual, 0.1);
+    }
+
+    @Test
+    public void testGetJob() {
+        Person person = new Person();
+        person.setJob("Programmer");
+
+        String expected = "Programmer";
+        String actual = person.getJob();
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testGetHobbies() {
+        Person person = new Person();
+        person.setHobbies(new String[]{"painting", "singing"});
+
+        String[] expected = {"painting", "singing"};
+        String[] actual = person.getHobbies();
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testGetGender() {
+        Person person = new Person();
+        person.setGender('F');
+
+        char expected = 'F';
+        char actual = person.getGender();
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testGetEyeColor() {
+        Person person = new Person();
+        person.setEyeColor("Blue");
+
+        String expected = "Blue";
+        String actual = person.getEyeColor();
+        Assert.assertEquals(actual,expected);
     }
 }
